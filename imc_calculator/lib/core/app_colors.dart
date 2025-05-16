c952914b2eba73c96eee1534833485f6afc4450f
kba1986cr <haroldsa1986@hotmail.com>
2025-05-15 22:57:29 -0600

    imc_calculator estructura + colores

---
+++ imc_calculator/lib/core/app_colors.dart
@@ -0,0 +1,25 @@
+import 'dart:ui';
+
+class AppColors {
+  static const Color primary = Color(0xFF6200EA);
+  static const Color secondary = Color(0xFF1f0345);
+  static const Color accent = Color(0xFFFFC400);
+
+  //Background colors
+  static const Color background = Color(0xFF0E0B20);
+  static const Color backgroundComponent = Color(0xFF1D1E33);
+  static const Color backgroundComponetSelected = Color(0xFF4F548B);
+
+  // static const Color primaryVariant = Color(0xFF3700B3);
+  // static const Color secondaryVariant = Color(0xFF018786);
+  // static const Color surface = Color(0xFFFFFFFF);
+  // static const Color error = Color(0xFFB00020);
+  // static const Color onPrimary = Color(0xFFFFFFFF);
+  // static const Color onSecondary = Color(0xFF000000);
+  // static const Color onBackground = Color(0xFF000000);
+  // static const Color onSurface = Color(0xFF000000);
+  // static const Color onError = Color(0xFFFFFFFF);
+  //static const Color textPrimary = Color(0xFF212121);
+  // static const Color textSecondary = Color(0xFF757575);
+  // static const Color textHint = Color(0xFFBDBDBD);
+}
--- imc_calculator/lib/main.dart
+++ imc_calculator/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:imc_calculator/core/app_colors.dart';
 
 void main() {
   runApp(const MainApp());
@@ -11,8 +12,12 @@ class MainApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       home: Scaffold(
+        backgroundColor: AppColors.background,
         body: Center(
-          child: Text('Hello World!'),
+          child: Text(
+            'Hello World!',
+            style: TextStyle(color: Colors.white, fontSize: 24),
+          ),
         ),
       ),
     );
