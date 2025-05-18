6f42c11659a0b55a2ea07ef00dcb01eae6a76d9c
kba1986cr <haroldsa1986@hotmail.com>
2025-05-17 19:32:29 -0600

    IMC_Dos home_screen

--- imc_dos/lib/main.dart
+++ imc_dos/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:imc_dos/core/app_colors.dart';
+import 'package:imc_dos/screens/imc_home_screec.dart';
 
 void main() {
   runApp(const MainApp());
@@ -18,12 +19,7 @@ class MainApp extends StatelessWidget {
           title: Text("IMC CALCULATOR"),
         ),
         backgroundColor: AppColors.background,
-        body: Center(
-          child: Text(
-            'Hello World!',
-            style: TextStyle(color: Colors.white, fontSize: 24),
-          ),
-        ),
+        body: ImcHomeScreen(),
       ),
     );
   }
