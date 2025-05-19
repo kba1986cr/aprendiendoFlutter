d328bcc550130e16ec399dec44d4ded1faca78a8
kba1986cr <haroldsa1986@hotmail.com>
2025-05-18 20:31:59 -0600

    IMC_Dos fonts->text

--- imc_dos/lib/components/gender_selector.dart
+++ imc_dos/lib/components/gender_selector.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:imc_dos/core/app_colors.dart';
+import 'package:imc_dos/core/text_styles.dart';
 
 class GenderSelector extends StatefulWidget {
   const GenderSelector({super.key});
@@ -14,17 +16,65 @@ class _GenderSelectorState extends State<GenderSelector> {
   Widget build(BuildContext context) {
     return Row(
       children: [
-        Column(
-          children: [
-            //
-            //Image.asset("assets/images/male.png"),
-          ],
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16,
+              top: 16,
+              right: 16,
+              bottom: 8,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color:
+                    selectedGender == "Hombre"
+                        ? AppColors.backgroundComponetSelected
+                        : AppColors.backgroundComponent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    //
+                    Image.asset("assets/images/m.png", height: 100),
+                    SizedBox(height: 10),
+                    Text("Hombre".toUpperCase(), style: TextStyles.bodyText),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
-        Column(
-          children: [
-            //
-            //Image.asset("assets/images/female.png"),
-          ],
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.only(
+              left: 16,
+              top: 16,
+              right: 16,
+              bottom: 8,
+            ),
+            child: Container(
+              decoration: BoxDecoration(
+                color:
+                    selectedGender == "Mujer"
+                        ? AppColors.backgroundComponetSelected
+                        : AppColors.backgroundComponent,
+                borderRadius: BorderRadius.circular(10),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(20),
+                child: Column(
+                  children: [
+                    //
+                    Image.asset("assets/images/f.png", height: 100),
+                    SizedBox(height: 10),
+                    Text("Mujer".toUpperCase(), style: TextStyles.bodyText),
+                  ],
+                ),
+              ),
+            ),
+          ),
         ),
       ],
     );
---
+++ imc_dos/lib/core/text_styles.dart
@@ -0,0 +1,20 @@
+import 'dart:ui';
+import 'package:flutter/material.dart';
+
+class TextStyles {
+  static const TextStyle bodyText = TextStyle(
+    color: Colors.white,
+    fontSize: 20,
+  );
+
+  /*
+  static const String fontFamily = 'Poppins';
+  static const String fontFamilyBold = 'Poppins-Bold';
+  static const String fontFamilyMedium = 'Poppins-Medium';
+  static const String fontFamilyLight = 'Poppins-Light';
+  static const String fontFamilySemiBold = 'Poppins-SemiBold';
+  static const String fontFamilyThin = 'Poppins-Thin';
+  static const String fontFamilyBlack = 'Poppins-Black';
+  static const String fontFamilyExtraBold = 'Poppins-ExtraBold';
+  */
+}
