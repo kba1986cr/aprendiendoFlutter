0e4d8808f659e7b7a00ff6e1294fc3cf925e34eb
kba1986cr <haroldsa1986@hotmail.com>
2025-05-19 20:09:30 -0600

    imc_Dos heightSlector al home

--- imc_dos/lib/screens/imc_home_screec.dart
+++ imc_dos/lib/screens/imc_home_screec.dart
@@ -4,6 +4,8 @@ import 'package:flutter/material.dart';
 import 'package:imc_dos/components/gender_selector.dart';
 import 'package:imc_dos/components/height_selector.dart';
 import 'package:imc_dos/components/number_selector.dart';
+import 'package:imc_dos/core/app_colors.dart';
+import 'package:imc_dos/core/text_styles.dart';
 
 class ImcHomeScreen extends StatefulWidget {
   const ImcHomeScreen({super.key});
@@ -13,7 +15,7 @@ class ImcHomeScreen extends StatefulWidget {
 }
 
 class _ImcHomeScreenState extends State<ImcHomeScreen> {
-  String? selectedGender;
+  String selectedGender = "";
   double? selectedHeight;
   int selectedAge = 18;
   int selectedWeight = 70;
@@ -56,6 +58,26 @@ class _ImcHomeScreenState extends State<ImcHomeScreen> {
               },
             ),
           ],
+        ),
+        Spacer(),
+        Padding(
+          padding: const EdgeInsets.all(16),
+          child: SizedBox(
+            height: 60,
+            width: double.infinity,
+            child: ElevatedButton(
+              onPressed: () {},
+              style: ButtonStyle(
+                shape: WidgetStatePropertyAll(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                backgroundColor: WidgetStatePropertyAll(AppColors.primary),
+              ),
+              child: Text("Calcular IMC", style: TextStyles.bodyText),
+            ),
+          ),
         ),
       ],
     );
